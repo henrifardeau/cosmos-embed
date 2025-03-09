ae045b46cc39392ab1f5d3862759597a89771d68
Henri Fardeau <fardeauhenri@gmail.com>
2025-03-09 20:50:07 +0100

    deploy dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}, // Mock the `process.env` object
+  },
+  resolve: {
+    alias: {
+      // Add this line to resolve process
+      process: 'process/browser',
+    },
+  },
   build: {
     lib: {
       entry: 'src/main.tsx',
